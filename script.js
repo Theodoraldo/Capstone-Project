facdd03ece3c794246fd11136bf6160b383b661e
Theodore Osei Asimeng <theodoraldo@gmail.com>
2023-04-20 05:13:33 +0000

    Correct ES6 linters errors

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const menu = document.getElementById('hamburger');
 const closeMenu = document.querySelector('.links i');
 const showMenu = document.querySelector('.showMenu');
-const card = document.querySelector('.main-program');
+const speakers = document.querySelector('.experts');
 const navItems = document.querySelectorAll('.showMenu li a');
 
 menu.addEventListener('click', () => {
@@ -11,67 +11,119 @@ menu.addEventListener('click', () => {
 
 closeMenu.addEventListener('click', () => {
   showMenu.style.display = 'absolute';
-  showMenu.style.left = '-100%'
+  showMenu.style.left = '-100%';
 });
 
-
-const cardData = [
+const speakerData = [
   {
     id: 0,
-    cardHead: 'Causes',
-    cardImg: './images/incoming_data.svg',
-    content: 'The risk of a cholera epidemic is highest when poverty, war or natural disasters force people to live in crowded conditions without adequate sanitation.',
+    personImg: './images/AnnahAsamoah.png',
+    personName: 'Dr. Annah Asamoah',
+    placeofWork: 'Okomfo Anokye Teaching Hospital',
+    duties: 'Department of Clinical Microbiology, Virology/Serology Unit. She is currently working on the identification of a second strain of extensively drug-resistant (XDR) gonorrhea.',
   },
   {
     id: 1,
-    cardHead: 'Symptoms',
-    cardImg: './images/shaking.svg',
-    content: 'Symptoms cause mild or moderate diarrhea are often hard to tell. Others develop more-serious signs usually within a few days of infection.',
+    personImg: './images/AsareBediako.png',
+    personName: 'Dr. Asare Bediako',
+    placeofWork: 'Ridge Government Hospital',
+    duties: 'Gastroenterologists who has specialized in abdominal pain, ulcers, diarrhea, jaundice, or cancers in your digestive organs. He also do a colonoscopy and other tests for colon cancer.',
   },
   {
     id: 2,
-    cardHead: 'Treatments',
-    cardImg: './images/treatment.svg',
-    content: 'Cholera patients should be evaluated and treated quickly. With proper treatment, even severely ill patients can be saved.',
+    personImg: './images/FeliciaAgoka.png',
+    personName: 'Dr. Felicia Agoka',
+    placeofWork: '37 Military Hospital',
+    duties: 'Cares for the whole family from children to the elderly, giving basic care. She does routine check-ups and screening tests, attending to ongoing medical conditions.',
   },
   {
     id: 3,
-    cardHead: 'Prone Areas',
-    cardImg: './images/globe.svg',
-    content: 'The risk of a cholera epidemic is highest when poverty, war or natural disasters force people to live in crowded conditions without adequate sanitation.',
+    personImg: './images/FrankAdjei.png',
+    personName: 'Dr. Frank Adjei',
+    placeofWork: 'Okomfo Anokye Teaching Hospital',
+    duties: 'An endocrinologist who treats the endocrine system. The glands producing and secreting hormones that regulate and control almost all of the bodys functions.',
+  },
+  {
+    id: 4,
+    personImg: './images/PriscillaAryee.png',
+    personName: 'Dr. Priscilla Aryee',
+    placeofWork: 'Korle-Bu Teaching Hospital',
+    duties: 'Infectious Disease Specialists who diagnose and treat infections in any part of your body, like fevers, Lyme disease, pneumonia, tuberculosis, and HIV and AIDS.',
+  },
+  {
+    id: 5,
+    personImg: './images/MariamOsei.png',
+    personName: 'Mrs Mariam Osei',
+    placeofWork: 'Fiber Systems Engineer at Vodafone Ghana',
+    duties: 'She designs, develops, and implements systems and databases to access and store geospatial data, analyze spatial and non-spatial information.',
   },
 ];
 
+speakers.innerHTML = `
+<div class="experts-details">
+<img src="${speakerData[0].personImg}" alt="Dr Annah" />
+<div class="experts-info">
+  <h3 class="experts-name">${speakerData[0].personName}</h3>
+  <p class="experts-place">${speakerData[0].placeofWork}</p>
+  <p class="experts-achieve">${speakerData[0].duties}
+  </p>
+</div>
+</div>
 
-card.innerHTML = `
-<div class="main-details">
-<img src="${cardData[0].cardImg}" alt="Logo" />
-<h3>${cardData[0].cardHead}</h3 >
-  <p>${cardData[0].content}</p>
-</div >
+<div class="experts-details">
+<img src="${speakerData[1].personImg}" alt="Dr Annah" />
+<div class="experts-info">
+  <h3 class="experts-name">${speakerData[1].personName}</h3>
+  <p class="experts-place">${speakerData[1].placeofWork}</p>
+  <p class="experts-achieve">${speakerData[1].duties}
+  </p>
+</div>
+</div>
 
-<div class="main-details">
-<img src="${cardData[1].cardImg}" alt="Logo" />
-<h3>${cardData[1].cardHead}</h3 >
-  <p>${cardData[1].content}</p>
-</div >
+<div class="experts-details">
+<img src="${speakerData[2].personImg}" alt="Dr Annah" />
+<div class="experts-info">
+  <h3 class="experts-name">${speakerData[2].personName}</h3>
+  <p class="experts-place">${speakerData[2].placeofWork}</p>
+  <p class="experts-achieve">${speakerData[2].duties}
+  </p>
+</div>
+</div>
 
-<div class="main-details">
-<img src="${cardData[2].cardImg}" alt="Logo" />
-<h3>${cardData[2].cardHead}</h3 >
-  <p>${cardData[2].content}</p>
-</div >
+<div class="experts-details">
+<img src="${speakerData[3].personImg}" alt="Dr Annah" />
+<div class="experts-info">
+  <h3 class="experts-name">${speakerData[3].personName}</h3>
+  <p class="experts-place">${speakerData[3].placeofWork}</p>
+  <p class="experts-achieve">${speakerData[3].duties}
+  </p>
+</div>
+</div>
 
-<div class="main-details">
-<img src="${cardData[3].cardImg}" alt="Logo" />
-<h3>${cardData[3].cardHead}</h3 >
-  <p>${cardData[3].content}</p>
-</div >
-  `
+<div class="experts-details">
+<img src="${speakerData[4].personImg}" alt="Dr Annah" />
+<div class="experts-info">
+  <h3 class="experts-name">${speakerData[4].personName}</h3>
+  <p class="experts-place">${speakerData[4].placeofWork}</p>
+  <p class="experts-achieve">${speakerData[4].duties}
+  </p>
+</div>
+</div>
+
+<div class="experts-details">
+<img src="${speakerData[5].personImg}" alt="Dr Annah" />
+<div class="experts-info">
+  <h3 class="experts-name">${speakerData[5].personName}</h3>
+  <p class="experts-place">${speakerData[5].placeofWork}</p>
+  <p class="experts-achieve">${speakerData[5].duties}
+  </p>
+</div>
+</div>
+  `;
 
 navItems.forEach((item) => {
   item.addEventListener('click', () => {
     showMenu.style.display = 'absolute';
-    showMenu.style.left = '-100%'
+    showMenu.style.left = '-100%';
   });
 });
